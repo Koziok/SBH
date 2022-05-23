79948c00fd3da34fd56f7b8356fb2275aa2b92e5
Koziok <69522734+Koziok@users.noreply.github.com>
2022-05-23 13:05:45 +0200

    Add greedy algorithm

--- SBH.py
+++ SBH.py
@@ -1,3 +1,4 @@
+from itertools import count
 from generate import generate
 import math
 import random
@@ -34,14 +35,14 @@ def addEdge(matrix, node1, node2, weight):
 
 def checkCov(word1, word2):
     if (word1 == word2):
-        return 0
+        return len(word1)
     else:
         for i in range(1, len(word1)):
             newWord1 = word1[i:]
             newWord2 = word2[:len(word1)-i]
             if(newWord1 == newWord2):
                 return i
-        return 0        
+        return len(word1)        
 
 def createGraph(oligo):
     numOfNodes = len(oligo)
@@ -55,6 +56,35 @@ def createGraph(oligo):
                 #print('Adding ', oligo[i], '--', cov, '-->', oligo[j])
     return adjMatrix
 
+def findMinimalNext(spectGraph, visited, currNode):
+    return 0
+
+def greedy(spectGraph, oligo, initNodeIndex, initNode, maxLen):
+    currNode = initNodeIndex
+    currLen = len(initNode)
+    currSeq = initNode
+    visited = []
+    visited.append(initNode)
+    while (currLen < maxLen):
+        weight = 100000000
+        chosenNode = 0
+        for i in range(0, len(oligo)):
+            if (oligo[i] not in visited):
+                minWeight = 100000000
+                for j in range(0, len(oligo)):
+                    if (oligo[j] not in visited):
+                        if (minWeight >= spectGraph[i][j]):
+                            minWeight = spectGraph[i][j]
+                if (weight >= (minWeight+spectGraph[currNode][i])):
+                    weight = minWeight+spectGraph[currNode][i]
+                    chosenNode = i
+        currLen += (spectGraph[currNode][chosenNode])
+        currSeq += oligo[chosenNode][-spectGraph[currNode][chosenNode]:]
+        currNode = chosenNode
+        visited.append(oligo[currNode])
+    return currSeq
+    
+
 if __name__ == '__main__':
     k = 4
     #errors = "positive"
@@ -62,20 +92,17 @@ if __name__ == '__main__':
     errors = "both"
     sequence = readFile('sequence.txt')
     oligo = portionDNA(sequence, k)
-    spectLen = len(oligo)
     initNode = oligo[0]
-    oligo = addErrors(oligo, errors, k)
+    #oligo = addErrors(oligo, errors, k)
     oligo.sort()
     oligo = list(dict.fromkeys(oligo))
-    print(initNode, oligo)
+    #print(initNode, oligo)
     spectGraph = createGraph(oligo)
     initNodeIndex = 0
     for i in range(len(oligo)):
         if(oligo[i] == initNode):
-            initNodeIndex = i
-    print('Coverages for ', initNode,':')
-    for i in range(len(oligo)):
-        if (oligo[i] == initNode):
-            print(oligo[i], ': Same node...')
-        else:
-            print(oligo[i], ': ', spectGraph[initNodeIndex][i])
+            initNodeIndex = i   
+    greedySeq = greedy(spectGraph, oligo, initNodeIndex, initNode, len(sequence))
+    #greedySeq = greedy(spectGraph, oligo, initNodeIndex, initNode, 10)
+    print(sequence)
+    print(greedySeq)
